617c527bc9df1bb475e07363188fee59d611c5be
LouisAntoninLesieur <louisantonin.lesieur@icloud.com>
2024-10-18 15:35:04 +0200

    remove sequelize connection validation

--- database.js
+++ database.js
@@ -5,7 +5,3 @@ import { Sequelize} from "sequelize";
 export const sequelize = new Sequelize(process.env.PG_URL,{
   dialect: "postgres",
 });
-
-sequelize.authenticate()
-      console.log("c'est good");
-      
