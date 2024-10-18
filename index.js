66cc57184a67ae4b6c466643ff34e98ae300c9fc
LouisAntoninLesieur <louisantonin.lesieur@icloud.com>
2024-10-18 23:14:02 +0200

    fix: manage router import

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import 'dotenv/config'
 import express from 'express';
-import { router } from './routers/router.js';
+import { router as apiRouter } from './routers/index.js'
 
 const app = express();
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
-app.use(router)
+app.use('/api', apiRouter)
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {
