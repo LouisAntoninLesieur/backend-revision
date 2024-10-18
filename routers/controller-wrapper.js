786c7c1959afd0ddfb133b1132f24d3ac322c222
LouisAntoninLesieur <louisantonin.lesieur@icloud.com>
2024-10-19 00:07:47 +0200

    add comments

--- routers/controller-wrapper.js
+++ routers/controller-wrapper.js
@@ -1,4 +1,8 @@
-/* This file writes a function that wraps a controller when it is called, i.e that any disfunction in the controller being called will be catched and return an error */
+/* 
+This file writes a function that wraps a controller when it is called, i.e that any disfunction in the controller being called will be catched and return an error 
+This allows us to have a centralised error handling system and avoid try/catches in controllers, making them easier to read and debug
+*/
+
 export function controllerWrapper(mdw) {
   return async (req, res, next) => {
     try {
@@ -8,4 +12,4 @@ export function controllerWrapper(mdw) {
       res.status(500).json({ error: "Unexpected server error. Please try again later while we are checking our Pokeballs." });
     }
   };
-}
+};
